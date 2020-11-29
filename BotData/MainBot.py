d9f81b6f13e83d4527962692ca530d01fe74ef1c
SOUFIANEZAZA <72149789+SOUFIANEZAZA@users.noreply.github.com>
2020-11-29 09:01:48 +0100

    Update MainBot.py

--- BotData/MainBot.py
+++ BotData/MainBot.py
@@ -205,9 +205,9 @@ def ig_directmessage():
     print("\u001b[33;1m-------------------------------------------------------\u001b[0m")
     while True:
         try:
-            USER = bot.get_hashtag_users(hashtag="CASABLANCA")
+            USER = bot.get_FOLLOWERS_users(FOLLOWERS="SIMOLIFEDOTCOM")
             USER_IDD = random.choice(USER)
-            NAME = bot.get_username_from_user_id(@SOUFIANE___FIT)
+            NAME = bot.get_username_from_user_id(USER_IDD)
             FULL_TEXT = ("Hi "+NAME+", "+INUsrp_TEXT)
             bot.send_profile(profile_user_id=bot.user_id,user_ids=USER_IDD,text=FULL_TEXT)
             bot.logger.info("Message Sented to "+ NAME)
