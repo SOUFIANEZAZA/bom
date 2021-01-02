b621a206dedff10526f9a923e231bfe6877f1e52
SOUFIANEZAZA <72149789+SOUFIANEZAZA@users.noreply.github.com>
2021-01-02 07:12:35 +0100

    Update MainBot.py

--- BotData/MainBot.py
+++ BotData/MainBot.py
@@ -88,7 +88,7 @@ def ig_masslooker():
                     bot.logger.info("Total stories viewed: %d" % bot.total["stories_viewed"])
 
                 # CHOOSE RANDOM LIKER TO GRAB HIS LIKERS AND REPEAT
-                countdown(240)
+                countdown(1000)
             except Exception as e:
                 # If something went wrong - sleep long and start again
                 bot.logger.info(e)
@@ -211,7 +211,7 @@ def ig_directmessage():
             FULL_TEXT = ("مرحبا كيف حالك "+NAME+", "+INUsrp_TEXT)
             bot.send_profile(profile_user_id=bot.user_id,user_ids=USER_IDD,text=FULL_TEXT)
             bot.logger.info("Message Sented to "+ NAME)
-            countdown(1500)
+            countdown(1000)
         except KeyboardInterrupt:
             break
             clear()
